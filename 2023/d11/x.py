edd12b70709458feee3df82691c0224cc813abac
francorbacho <francorbacho@proton.me>
2023-12-11 11:17:47 +0100

    add aoc 23/11 p2
    this is pretty bad, takes around 3 min to execute

--- 2023/d11/x.py
+++ 2023/d11/x.py
@@ -36,8 +36,29 @@ def calculate_dist(src: tuple[int, int], dst: tuple[int, int]) -> int:
     dist = abs(src[0] - dst[0]) + abs(src[1] - dst[1])
     return dist
 
-def part1():
-    lines = [line.strip() for line in fileinput.input() if line.strip() != ""]
+def calculate_dist_p2(world: list[list[str]], src: tuple[int, int], dst: tuple[int, int]) -> int:
+    EXPAND = 10
+    EXPAND = 100
+    EXPAND = 1_000_000
+
+    dist = abs(src[0] - dst[0]) + abs(src[1] - dst[1])
+
+    # calculate how many empty rows we go by
+    for row in range(min(src[0], dst[0]), max(src[0], dst[0])):
+        line = "".join(world[row])
+        if "#" not in line:
+            dist += EXPAND - 1
+
+    # calculate how many empty cols we go by
+    world = np.array(world)
+    for col in range(min(src[1], dst[1]), max(src[1], dst[1])):
+        line = "".join(world[:, col])
+        if "#" not in line:
+            dist += EXPAND - 1
+
+    return dist
+
+def part1(world: list[list[str]]):
     world = expand(lines)
     galax = count_galaxies(world)
     dists = [calculate_dist(*pair) for pair in itertools.combinations(galax, 2)]
@@ -46,4 +67,16 @@ def part1():
 
     print(f"res :: {res}")
 
-part1()
+def part2(world: list[str]):
+    world = [[*line] for line in world]
+    galax = count_galaxies(world)
+    dists = [calculate_dist_p2(world, *pair) for pair in itertools.combinations(galax, 2)]
+
+    res = sum(dists)
+
+    print(f"res :: {res}")
+
+lines = [line.strip() for line in fileinput.input() if line.strip() != ""]
+
+part1(lines)
+part2(lines)
